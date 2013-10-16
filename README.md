1e589d13d3f4549a458345d458e155035928a2b0
Keisuke KITA <kei.kita2501@gmail.com>
2013-10-16 09:55:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::ColorMeShop - It's new $module
+
+# SYNOPSIS
+
+    use WebService::ColorMeShop;
+
+# DESCRIPTION
+
+WebService::ColorMeShop is ...
+
+# LICENSE
+
+Copyright (C) Keisuke KITA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Keisuke KITA <kei.kita2501@gmail.com>
---
+++ lib/WebService/ColorMeShop.pm
@@ -0,0 +1,39 @@
+package WebService::ColorMeShop;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::ColorMeShop - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::ColorMeShop;
+
+=head1 DESCRIPTION
+
+WebService::ColorMeShop is ...
+
+=head1 LICENSE
+
+Copyright (C) Keisuke KITA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Keisuke KITA E<lt>kei.kita2501@gmail.comE<gt>
+
+=cut
+
