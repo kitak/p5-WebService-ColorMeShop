3271e3086092a6c41ce0558cd43ed10185ef27de
Keisuke KITA <kei.kita2501@gmail.com>
2013-10-20 12:38:09 +0900

    implements some update interfaces

--- lib/WebService/ColorMeShop.pm
+++ lib/WebService/ColorMeShop.pm
@@ -3,10 +3,11 @@ use 5.008005;
 use strict;
 use warnings;
 use utf8;
+use JSON;
 use WebService::Simple;
 
 our $VERSION = "0.01";
-our $BASE_URL = 'https://api.shop-pro.jp';
+our $BASE_URL = 'https://api.shop-pro.jp/v1';
 
 sub new {
   my ($class, %option) = @_;
@@ -26,96 +27,129 @@ sub new {
 
 sub shop {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/shop.json', {});
+  my $res = $self->{_agent}->get('/shop.json', {});
   return %{ $res->parse_response->{shop} };
 }
 
 sub sales_stat {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/sales/stat.json', {});
+  my $res = $self->{_agent}->get('/sales/stat.json', {});
   return %{ $res->parse_response->{sales_stat} };
 }
 
 sub sales {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/sales.json', {});
+  my $res = $self->{_agent}->get('/sales.json', {});
   return @{ $res->parse_response->{sales} };
 }
 
 sub sale {
   my $self = shift;
   my $id = shift;
-  my $res = $self->{_agent}->get("/v1/sales/${id}.json", {});
+  my $res = $self->{_agent}->get("/sales/${id}.json", {});
   return %{ $res->parse_response->{sale} };
 }
 
-sub update_safe {
+sub update_sale {
+  my $self = shift;
+  my $id = shift;
+  my %param = @_;
+
+  if (exists $param{paid} && $param{paid}) {
+    $param{paid} = JSON::true;
+  } else {
+    $param{paid} = JSON::false;
+  }
+  for my $sale_delivery (@{ $param{"sale_deliveries"} }){
+    if (exists $sale_delivery->{delivered} && $sale_delivery->{delivered}) {
+      $sale_delivery->{delivered} = JSON::true;
+    } else {
+      $sale_delivery->{delivered} = JSON::false;
+    }
+  }
+  my $res = $self->{_agent}->get("/sales/${id}.json", {
+    sale => \%param
+  }, "X-Http-Method-Override" => "put");
+  return %{ $res->parse_response->{sale} };
 }
 
 sub customers {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/customers.json', {});
+  my $res = $self->{_agent}->get('/customers.json');
   return @{ $res->parse_response->{customers} };
 }
 
 sub customer {
   my $self = shift;
   my $id = shift;
-  my $res = $self->{_agent}->get("/v1/customers/${id}.json", {});
+  my $res = $self->{_agent}->get("/customers/${id}.json");
   return %{ $res->parse_response->{customer} };
 }
 
 sub products {
   my $self = shift;
   my %params = @_;
-  my $res = $self->{_agent}->get('/v1/products.json', %params);
+  my $res = $self->{_agent}->get('/products.json', %params);
   return @{ $res->parse_response->{products} };
 }
 
 sub update_product {
+  my $self = shift;
+  my $id = shift;
+  my %param = @_;
+
+  if (exists $param{stock_managed} && $param{stock_manages}) {
+    $param{paid} = JSON::true;
+  } else {
+    $param{paid} = JSON::false;
+  }
+  my $res = $self->{_agent}->post("/sales/${id}.json", {
+    product => \%param
+  }, "X-Http-Method-Override" => "put");
+  return %{ $res->parse_response->{sale} };
 }
 
 sub product {
   my $self = shift;
   my $id = shift;
-  my $res = $self->{_agent}->get("/v1/products/${id}.json", {});
+  my $res = $self->{_agent}->get("/products/${id}.json");
   return %{ $res->parse_response->{product} };
 }
 
 sub stocks {
   my $self = shift;
   my %params = @_;
-  my $res = $self->{_agent}->get('/v1/stocks.json', %params);
+  my $res = $self->{_agent}->get('/stocks.json', %params);
   return @{ $res->parse_response->{stocks} };
 }
 
 sub categories {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/categories.json', {});
+  my $res = $self->{_agent}->get('/categories.json');
   return @{ $res->parse_response->{categories} };
 }
 
 sub payments {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/payments.json', {});
+  my $res = $self->{_agent}->get('/payments.json');
   return @{ $res->parse_response->{payments} };
 }
 
 sub deliveries {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/deliveries.json', {});
+  my $res = $self->{_agent}->get('/deliveries.json');
   return @{ $res->parse_response->{deliveries} };
 }
 
 sub deliveries_date {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/deliveries/date.json', {});
+  my $res = $self->{_agent}->get('/deliveries/date.json');
   return %{ $res->parse_response->{delivery_date} };
 }
 
 sub gifts {
   my $self = shift;
-  my $res = $self->{_agent}->get('/v1/gifts.json', {});
+  my $res = $self->{_agent}->get('/gifts.json');
   return @{ $res->parse_response->{gifts} };
 }
 
@@ -126,15 +160,49 @@ __END__
 
 =head1 NAME
 
-WebService::ColorMeShop - It's new $module
+WebService::ColorMeShop - A Perl interface to ColorMeShop API
 
 =head1 SYNOPSIS
 
     use WebService::ColorMeShop;
 
+    my $colorme = WebService::ColorMeShop->new(
+        access_token => 'YOUR ACCESS TOKEN'
+    )
+
+    # retrieve shop data
+    my $shop = $colorme->shop;
+
+    # retrieve categories data
+    my $categories = $colorme->categories;
+
 =head1 DESCRIPTION
 
-WebService::ColorMeShop is ...
+ColorMeShop is a website ... WebService::ColorMeShop provides
+ an easy way to communicate with it using its API.
+
+=head1 METHODS
+
+=head2 new ( I<%args> )
+
+=over 4
+
+    my $colorme = WebService::ColorMeShop->new(
+        access_token => 'YOUR SECRET TOKEN'
+    )
+
+This method creates and returns a new WebService::ColorMeShop 
+object.
+
+=back
+
+=head2 shop
+
+=over 4
+
+    $colorme->shop
+
+This method retrieves the data of the shop and returns a hash to them.
 
 =head1 LICENSE
 
