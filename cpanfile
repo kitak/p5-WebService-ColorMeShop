b79f1c974ca14d5f87021e93a5775d29d3997414
Keisuke KITA <kei.kita2501@gmail.com>
2013-10-26 17:43:32 +0900

    WebService::Simpleの代わりにLWP::UserAgentを使う

--- cpanfile
+++ cpanfile
@@ -1,7 +1,9 @@
 requires 'perl', '5.008001';
-requires 'WebService::Simple';
+requires 'LWP::UserAgent';
 requires 'LWP::Protocol::https';
 requires 'JSON';
+requires 'HTTP::Request';
+requires 'Class::Accessor::Lite';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
--- lib/WebService/ColorMeShop.pm
+++ lib/WebService/ColorMeShop.pm
@@ -3,158 +3,199 @@ use 5.008005;
 use strict;
 use warnings;
 use utf8;
+use Carp;
+use LWP::UserAgent;
+use Http::Request;
 use JSON;
-use WebService::Simple;
+use Class::Accessor::Lite (
+    rw => [ qw/access_token/ ],
+);
 
 our $VERSION = "0.01";
 our $BASE_URL = 'https://api.shop-pro.jp/v1/';
 
 sub new {
-  my ($class, %option) = @_;
-  my $access_token = $option{'access_token'};
-  my $agent = WebService::Simple->new(
-    base_url        => $BASE_URL,
-    response_parser => 'JSON',
-  );
-  $agent->add_handler(request_send => sub {
-    my ($req, $agent) = @_;
-    $req->header('Authorization' => "Bearer ${access_token}");
-    return;
-  });
+    my ($class, %arg) = @_;
 
-  bless {_agent => $agent}, $class;
+    bless {
+        agent => $arg{aagent} || LWP::UserAgent->new(agent => "WebService::ColorMeShop/${VERSION}"),
+        access_token => $arg{access_token},
+    }, $class;
+}
+
+sub _get {
+    my $self = shift;
+    my $path = shift;
+    my %param = @_;
+
+    my $uri = URI->new("${BASE_URL}${path}");
+    $uri->query_form(%param);
+    my $req = HTTP::Request->new('GET', $uri->as_string,
+        [Authorization => "Bearer $self->access_token"],
+    );
+
+    my $res = $self->{agent}->request($req);
+    $self->_error_hundler($res);
+    return decode_json($res->content);
+}
+
+sub _put {
+    my $self = shift;
+    my $path = shift;
+    my $param = shift;
+
+    my $uri = URI->new("${BASE_URL}${path}");
+    my $req = HTTP::Request->new('POST', $uri->as_string,
+        [Authorization => "Bearer $self->access_token",
+         "X-Http-Method-Override" => "put",
+         "Content-Type"           => "application/json"],
+        encode_json($param)
+    );
+
+    my $res = $self->{agent}->request($req);
+    $self->_error_hundler($res);
+    return decode_json($res->content);
+}
+
+sub _error_hundler {
+    my $self = shift;
+    my $res  = shift;
+
+    if (int($res->code / 100) == 4) {
+        croak("API error\n${res->content}")
+    }
 }
 
 sub shop {
-  my $self = shift;
-  my $res = $self->{_agent}->get('shop.json', {});
-  return %{ $res->parse_response->{shop} };
+    my $self = shift;
+    my $res = $self->_get('shop.json');
+    return $res->{shop};
 }
 
 sub sales_stat {
-  my $self = shift;
-  my $res = $self->{_agent}->get('sales/stat.json', {});
-  return %{ $res->parse_response->{sales_stat} };
+    my $self = shift;
+    my $res = $self->_get('sales/stat.json');
+    return $res->{sales_stat};
 }
 
 sub sales {
-  my $self = shift;
-  my %param = @_;
-  my $res = $self->{_agent}->get('sales.json', %param);
-  return @{ $res->parse_response->{sales} };
+    my $self = shift;
+    my %param = @_;
+    my $res = $self->_get('sales.json', %param);
+    return $res->{sales};
 }
 
 sub sale {
-  my $self = shift;
-  my $id = shift;
-  my $res = $self->{_agent}->get("sales/${id}.json", {});
-  return %{ $res->parse_response->{sale} };
+    my $self = shift;
+    my $id = shift;
+    my $res = $self->_get("sales/${id}.json");
+    return $res->{sale};
 }
 
 sub update_sale {
-  my $self = shift;
-  my $id = shift;
-  my %param = @_;
+    my $self = shift;
+    my $id = shift;
+    my %param = @_;
 
-  if (exists $param{paid} && $param{paid}) {
-    $param{paid} = JSON::true;
-  } elsif (exists $param{paid} && !$param{paid}) {
-    $param{paid} = JSON::false;
-  }
-  for my $sale_delivery (@{ $param{"sale_deliveries"} }){
-    if (exists $sale_delivery->{delivered} && $sale_delivery->{delivered}) {
-      $sale_delivery->{delivered} = JSON::true;
-    } else {
-      $sale_delivery->{delivered} = JSON::false;
+    if (exists $param{paid} && $param{paid}) {
+        $param{paid} = JSON::true;
+    } elsif (exists $param{paid} && !$param{paid}) {
+        $param{paid} = JSON::false;
     }
-  }
-  my $res = $self->{_agent}->get("sales/${id}.json", {
-    sale => \%param
-  }, "X-Http-Method-Override" => "put",
-     "Content-Type"           => "application/json");
-  return %{ $res->parse_response->{sale} };
+    if (exists $param{"sale_deliveries"}) {
+        for my $sale_delivery (@{ $param{"sale_deliveries"} }){
+            if (exists $sale_delivery->{delivered} && $sale_delivery->{delivered}) {
+                $sale_delivery->{delivered} = JSON::true;
+            } else {
+                $sale_delivery->{delivered} = JSON::false;
+            }
+        }
+    }
+    my $res = $self->_put("sales/${id}.json", {
+            sale => \%param
+    });
+    return $res->{sale};
 }
 
 sub customers {
-  my $self = shift;
-  my $res = $self->{_agent}->get('customers.json');
-  return @{ $res->parse_response->{customers} };
+    my $self = shift;
+    my $res = $self->_get('customers.json');
+    return $res->{customers};
 }
 
 sub customer {
-  my $self = shift;
-  my $id = shift;
-  my $res = $self->{_agent}->get("customers/${id}.json");
-  return %{ $res->parse_response->{customer} };
+    my $self = shift;
+    my $id = shift;
+    my $res = $self->_get("customers/${id}.json");
+    return $res->{customer};
 }
 
 sub products {
-  my $self = shift;
-  my %params = @_;
-  my $res = $self->{_agent}->get('products.json', %params);
-  return @{ $res->parse_response->{products} };
+    my $self = shift;
+    my %params = @_;
+    my $res = $self->_get('products.json', %params);
+    return $res->{products};
 }
 
 sub product {
-  my $self = shift;
-  my $id = shift;
-  my $res = $self->{_agent}->get("products/${id}.json");
-  return %{ $res->parse_response->{product} };
+    my $self = shift;
+    my $id = shift;
+    my $res = $self->_get("products/${id}.json");
+    return $res->{product};
 }
 
 sub update_product {
-  my $self = shift;
-  my $id = shift;
-  my %param = @_;
+    my $self = shift;
+    my $id = shift;
+    my %param = @_;
 
-  if (exists $param{stock_managed} && $param{stock_manages}) {
-    $param{paid} = JSON::true;
-  } else {
-    $param{paid} = JSON::false;
-  }
-  my $res = $self->{_agent}->post("sales/${id}.json", {
-    product => \%param
-  }, "X-Http-Method-Override" => "put");
-  return %{ $res->parse_response->{sale} };
+    if (exists $param{stock_managed} && $param{stock_manages}) {
+        $param{paid} = JSON::true;
+    } else {
+        $param{paid} = JSON::false;
+    }
+    my $res = $self->_put("sales/${id}.json", {
+            product => \%param
+    });
+    return $res->{sale};
 }
 
 sub stocks {
-  my $self = shift;
-  my %params = @_;
-  my $res = $self->{_agent}->get('stocks.json', %params);
-  return @{ $res->parse_response->{stocks} };
+    my $self = shift;
+    my %params = @_;
+    my $res = $self->_get('stocks.json', %params);
+    return $res->{stocks};
 }
 
 sub categories {
-  my $self = shift;
-  my $res = $self->{_agent}->get('categories.json');
-  return @{ $res->parse_response->{categories} };
+    my $self = shift;
+    my $res = $self->_get('categories.json');
+    return $res->{categories};
 }
 
 sub payments {
-  my $self = shift;
-  my $res = $self->{_agent}->get('payments.json');
-  return @{ $res->parse_response->{payments} };
+    my $self = shift;
+    my $res = $self->_get('payments.json');
+    return $res->{payments};
 }
 
 sub deliveries {
-  my $self = shift;
-  my $res = $self->{_agent}->get('deliveries.json');
-  return @{ $res->parse_response->{deliveries} };
+    my $self = shift;
+    my $res = $self->_get('deliveries.json');
+    return $res->{deliveries};
 }
 
 sub deliveries_date {
-  my $self = shift;
-  my $res = $self->{_agent}->get('deliveries/date.json');
-  return %{ $res->parse_response->{delivery_date} };
+    my $self = shift;
+    my $res = $self->_get('deliveries/date.json');
+    return $res->{delivery_date};
 }
 
 sub gifts {
-  my $self = shift;
-  my $res = $self->{_agent}->get('gifts.json');
-  return @{ $res->parse_response->{gifts} };
+    my $self = shift;
+    my $res = $self->_get('gifts.json');
+    return $res->{gifts};
 }
-
 1;
 __END__
 
